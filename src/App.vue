1e3373325b91385bd94700e32056f23fcadd598f
Dastan <daskov2412@gmail.com>
2023-11-01 16:44:49 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container d-flex flex-column">
+    <div class="header d-flex flex-row justify-center align-center">POSTS</div>
+    <div class="content d-flex flex-column">
+      <Loader :is-loading="isLoading" />
+      <v-list v-if="!isLoading">
+        <v-list-item v-for="post in posts" :key="post.id">
+          <h1>{{ post.title }}</h1>
+          <p>{{ post.body }}</p>
+          <v-divider />
+        </v-list-item>
+      </v-list>
+    </div>
+    <div class="footer d-flex flex-row justify-center align-center">
+      <v-btn variant="elevated" @click="toggleLoader">
+        {{ loaderButton }}
+      </v-btn>
+    </div>
+  </div>
 </template>
 
+<script setup>
+import {computed, ref} from 'vue';
+import Loader from '@/Loader.vue';
+
+const isLoading = ref(false);
+const posts = ref([]);
+const loaderButton = computed(() => isLoading.value ? 'Hide loader' : 'Show loader')
+
+const toggleLoader = () => {
+  isLoading.value = !isLoading.value;
+};
+
+const fetchData = async () => {
+  try {
+    isLoading.value = true;
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=5');
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    posts.value = await response.json();
+  } catch (error) {
+    console.error('Error while receiving data', error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+fetchData();
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
+.container {
+  min-height: 100vh;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.content {
+  flex: 1;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.header {
+  height: 60px;
+  background: aqua;
+  font-size: 40px;
+}
+
+.footer {
+  height: 60px;
+  background: aqua;
 }
 </style>
---
+++ src/Loader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="isLoading" class="loader"/>
+</template>
+
+<script setup>
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.loader {
+  border-top: 4px solid blue;
+  border-right: 4px solid blue;
+  border-bottom: 4px solid blue;
+  border-left: 4px solid #fff;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1.5s linear infinite;
+  margin: auto;
+  clip: rect(0px, 25px, 50px, 0px);
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
